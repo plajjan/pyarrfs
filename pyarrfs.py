4db3fc08dcf22b765eea65634167f3b12483f565
Kristian Larsson <kristian@spritelink.net>
2011-01-07 04:41:13 +0100

    Add pyarrfs.py in root for running uninstalled
    If someone wants to run pyarrfs without installing it can be done with
    this script (which I somehow forgot to include earlier).

---
+++ pyarrfs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#
+# PyarrFS - a RAR reading file system
+# Copyright (c) 2010, 2011 Kristian Larsson <kristian@spritelink.net>
+#
+# This file is license under the X11/MIT license, please see the file COPYING
+# distributed with PyarrFS for more details.
+#
+
+
+import sys
+
+import pyarrfs.pyarrfs
+try:
+    pyarrfs.pyarrfs.main()
+except KeyboardInterrupt:
+    pass
+sys.exit(0)
