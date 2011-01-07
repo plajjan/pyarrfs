e8853997b7ba28da48e1620fd1466fbe8ca1d0c0
Kristian Larsson <kristian@spritelink.net>
2011-01-07 04:23:10 +0100

    Add setup.py
    This has not been tested just yet ;)
    
    To comment on a previous commit.. version has been set to 0.5.0 which
    will probably be the first one to be packaged and made available. 0.5.0
    was chosen as this "beta" since I feel we are about halfway there to a
    good solid product (1.0.0).

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+from pyarrfs import pyarrfs
+
+ver = pyarrfs.__version__
+long_desc = open("README").read()
+short_desc = long_desc.split('\n')[0].split(' - ')[1].strip()
+
+setup(
+    name = 'pyarrfs',
+    version = pyarrfs.__version__,
+    description = short_desc,
+    long_description = long_desc,
+    author = pyarrfs.__author__,
+    license = pyarrfs.__license__,
+    author_email = pyarrfs.__author_email__,
+    url = pyarrfs.__url__,
+    scripts = ['pyarrfs'],
+    keywords = ['rar', 'fuse'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Topic :: System :: Archiving :: Compression',
+        'Topic :: System :: Filesystems'
+    ]
+)
+
